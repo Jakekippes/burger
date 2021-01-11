95aa02aabe42859846a8aa0e8c3b76da1bbb3c2d
Jake <jakekippes123gmail.com>
2021-01-11 14:20:45 -0600

    added the database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon and Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
