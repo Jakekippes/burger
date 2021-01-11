c29ffec2f54014860de0d92b77631260854fc695
Jake <jakekippes123gmail.com>
2021-01-11 14:41:29 -0600

    establishing connection with mysql

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Wow12345",
+    database: "burger_db",
+  });
+}
+
+connection.connect();
+
+module.exports = connection;
